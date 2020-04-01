e2f2fc9c8b30092698b427a52484d40313be5ca1
Jacob Wharton <jacob.wharton789@gmail.com>
2020-04-01 10:38:47 -0400

    fade effect working

--- js/javascript.js
+++ js/javascript.js
@@ -1,46 +1,64 @@
 
+var moreLess_busy = false;
+
 function printPage() {
+	var op = 1;
+    var element = document.getElementById("two-page-version-only");
+	element.style.opacity = op;
 	window.print();
 }
+
 function toggleOneTwoPage() {
 	var element = document.getElementById("two-page-version-only");
+	if (moreLess_busy)
+	{ return; }
+	moreLess_busy = true;
+	console.log("registered press.");
 	if (element.style.display === "none")
 	{ 
 		element.style.display = "block"; //
-		//fadeInEffect();
+		//element.style.opacity = 1; //
+		fadeInEffect();
 	}
 	else
 	{ 
-		element.style.display = "none"; //
-		//fadeOutEffect();
+		fadeOutEffect();
 	}
 }
 
 function fadeOutEffect() {
+	var op = 1;
     var element = document.getElementById("two-page-version-only");
+	element.style.opacity = op;
     var fadeEffect = setInterval(function () {
-		if (!element.style.opacity) { element.style.opacity = 1; }
 		if (element.style.opacity > 0)
-		{ element.style.opacity -= 0.05; } 
+		{
+			element.style.opacity = op;
+			op -= 0.05;
+		} 
 		else
 		{
             clearInterval(fadeEffect);
 			element.style.display = "none";
+			moreLess_busy = false;
         }
     }, 20);
 }
 function fadeInEffect() {
+	var op = 0;
     var element = document.getElementById("two-page-version-only");
 	element.style.display = "block";
     var fadeEffect = setInterval(function () {
-		console.log(element.style.opacity);
 		if (element.style.opacity < 1)
 		{ 
-			element.style.opacity += 0.05;
-			console.log("hi");
+			element.style.opacity = op;
+			op += 0.05;
 		}
 		else
-		{ clearInterval(fadeEffect); }
+		{ 
+			clearInterval(fadeEffect);
+			moreLess_busy = false; 
+		}
     }, 20);
 }
 
